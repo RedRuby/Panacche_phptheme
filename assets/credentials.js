84cabed91d9a4f8ee4ccbc2c14051259c2068cb4
nileshb <nileshbari.8085@gmail.com>
2021-04-27 17:07:39 +0530

    git ignore removed credentials.js

---
+++ assets/credentials.js
@@ -0,0 +1,55 @@
+var ngrokURL = "";
+var shop = window.location.hostname;
+if (shop == 'panaccheuat.myshopify.com') {
+    ngrokURL = "https://qodequaydev.com";
+} else {
+    ngrokURL = "https://2dd410e9d484.ngrok.io";
+}
+(function($) {
+    $(function() {
+        $(".alert-danger .close").on("click", function(e) {
+            e.preventDefault();
+            console.log("djbasdd");
+            $(".alert-danger").addClass('hide');
+            $("#shopify-section-toast-message").addClass('hide');
+        });
+
+        $(document).ajaxSend(function() {
+            $(".spinner-border").removeClass('hide');
+            $("#loadingDiv").removeClass('hide');
+            $("#shopify-section-toast-message").removeClass('hide');
+            console.log("start");
+        });
+
+        $(document).ajaxComplete(function() {
+            $(".spinner-border").addClass('hide');
+            $("#loadingDiv").addClass('hide');
+            console.log("complete");
+        });
+
+        $(document).ajaxError(function() {
+            $(".spinner-border").addClass('hide');
+            $("#loadingDiv").addClass('hide');
+            console.log("error");
+        });
+
+        $(document).ajaxStop(function() {
+            $(".spinner-border").addClass('hide');
+            $("#loadingDiv").addClass('hide');
+            console.log("stop");
+        });
+
+        $(document).ajaxSuccess(function() {
+            $(".spinner-border").addClass('hide');
+            $("#loadingDiv").addClass('hide');
+            console.log("success");
+        });
+
+        $(".alert-success .close").on("click", function(e) {
+            e.preventDefault();
+            console.log("hide success alert");
+            $(".alert-success").addClass('hide');
+            $("#shopify-section-toast-message").addClass('hide');
+        })
+    });
+})(jQuery);
